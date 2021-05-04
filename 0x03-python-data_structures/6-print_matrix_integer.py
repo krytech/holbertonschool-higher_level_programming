c5b6c78c3da59a472fd82b46f7361ea16f60d4ae
Carson Stearn <cstearn@gmail.com>
2021-05-04 23:56:02 +0000

    fix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for i in row:
-            if i != 0:
+            if i != row[- 1]:
                 print("{:d}".format(i), end=" ")
             else:
                 print("{:d}".format(i), end="")
