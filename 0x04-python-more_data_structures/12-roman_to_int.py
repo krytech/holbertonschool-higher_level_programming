753eabb83f16f249e5a2b6ebaa6062ec218a824a
Carson Stearn <cstearn@gmail.com>
2021-05-06 21:53:05 +0000

    task 11, updated 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,8 +4,16 @@ def roman_to_int(roman_string):
         return 0
     dict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     sum = 0
+    next = ""
     for i in range(len(roman_string)):
         if roman_string[i] in dict.keys():
             val = roman_string[i]
-            sum += dict[val]
+            if i < len(roman_string) - 1:
+                next = roman_string[i + 1]
+            if i == len(roman_string) - 1:
+                sum += dict[val]
+            elif dict[val] < dict[next]:
+                sum -= dict[val]
+            else:
+                sum += dict[val]
     return sum
