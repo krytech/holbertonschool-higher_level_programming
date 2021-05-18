8a62f5e951544b70bf5e13aa1c97c927cf569955
Carson Stearn <cstearn@gmail.com>
2021-05-18 21:00:19 +0000

    task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""A simple class definition for a Square"""
+
+
+class Square:
+    """Class Square definition"""
+    def __init__(self, size=0):
+        """Initializes Square"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Calculates area of the square"""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """Getter"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter"""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def my_print(self):
+        """prints in stdout the square with the character #"""
+        square_size = self.__size
+        if square_size == 0:
+            print()
+        for i in range(square_size):
+            for j in range(square_size):
+                print("#", end="")
+            print()
