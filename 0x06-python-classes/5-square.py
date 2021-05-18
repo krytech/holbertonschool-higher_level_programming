9859de63cf93f5fa3e7f3a853896ad305cff54e8
Carson Stearn <cstearn@gmail.com>
2021-05-18 22:51:23 +0000

    assignment fix

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -6,12 +6,7 @@ class Square:
     """Class Square definition"""
     def __init__(self, size=0):
         """Initializes Square"""
-        if type(size) != int:
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
 
     def area(self):
         """Calculates area of the square"""
--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -6,13 +6,8 @@ class Square:
     """Class Square definition"""
     def __init__(self, size=0, position=(0, 0)):
         """Initializes Square"""
-        if type(size) != int:
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.size = size
-            self.position = position
+        self.size = size
+        self.position = position
 
     def area(self):
         """Calculates area of the square"""
