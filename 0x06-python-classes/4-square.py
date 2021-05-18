18758222636dd5826eb9932dd89e7646b70c45ea
Carson Stearn <cstearn@gmail.com>
2021-05-18 20:35:48 +0000

    """

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+"""A simple class definition for a Square"""
+
+
 class Square:
+    """Class Square definition"""
     def __init__(self, size=0):
+        """Initializes Square"""
         if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
@@ -9,14 +14,17 @@ class Square:
             self.__size = size
 
     def area(self):
+        """Calculates area of the square"""
         return self.__size ** 2
 
     @property
     def size(self):
+        """Getter"""
         return self.__size
 
     @size.setter
     def size(self, value):
+        """Setter"""
         if type(value) != int:
             raise TypeError("size must be an integer")
         elif value < 0:
