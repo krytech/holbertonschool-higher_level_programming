1c9f73d8699f3c18b65afa7fd04c8e1c5ddec228
Carson Stearn <cstearn@gmail.com>
2021-05-18 20:17:48 +0000

    """

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""A simple class definition for a Square"""
+
+
 class Square:
+    """Class Square definition"""
     def __init__(self, size):
+        """Initializes Square"""
         self.__size = size
