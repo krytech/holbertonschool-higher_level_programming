d6a04095c9a2899055925312fabd1a062f39b14e
Carson Stearn <cstearn@gmail.com>
2021-06-02 22:13:04 +0000

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Creates an class called BaseGeometry with a public instance
+method for the area
+"""
+
+
+class BaseGeometry:
+    """ Class: BaseGeometry with area """
+    def area(self):
+        """ Area not yet implemented """
+        raise Exception("area() is not implemented")
