7044fc0514b1392ef54909fb53d5bd43394584c9
Carson Stearn <cstearn@gmail.com>
2021-08-04 03:08:38 +0000

    task 0-10

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let row = 0; row < this.height; row += 1) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+};
