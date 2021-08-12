25e300ff842856f979c5790a554f8e0306cde317
Carson Stearn <cstearn@gmail.com>
2021-08-12 14:09:52 +0000

    init, task 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+A python script that lists all 'states' from the
+database 'hbtn_0e_0_usa'
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=argv[1], passwd=argv[2],
+                         db=argv[3])
+
+cursor = db.cursor()
+cursor.execute("SELECT * FROM states ORDER BY id ASC")
+for row in cursor.fetchall():
+    print(row)
+cursor.close()
+db.close()
