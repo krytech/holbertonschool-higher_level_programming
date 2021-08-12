2571f25f643491e78836821fbf5e624aaab8c260
Carson Stearn <cstearn@gmail.com>
2021-08-12 14:24:55 +0000

    spacing

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -12,9 +12,9 @@ if __name__ == "__main__":
                          user=argv[1], passwd=argv[2],
                          db=argv[3])
 
-cursor = db.cursor()
-cursor.execute("SELECT * FROM states ORDER BY id ASC")
-for row in cursor.fetchall():
-    print(row)
-cursor.close()
-db.close()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    for row in cursor.fetchall():
+        print(row)
+    cursor.close()
+    db.close()
