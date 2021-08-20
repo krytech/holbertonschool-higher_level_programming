c35642a70cde04b188b4f868aa0ab36a281d0fba
Carson Stearn <cstearn@gmail.com>
2021-08-20 20:26:24 +0000

    adv

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Use Github API to list last 10 commits given a repo name and owner
+"""
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
+    r = requests.get(url)
+    commits = r.json()
+
+    for i in range (10):
+        print("{}: {}".format(commits[i].get("sha"),
+        commits[i].get("commit").get("author").get("name")))
