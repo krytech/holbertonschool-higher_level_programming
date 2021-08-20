08c798fc44bbb15ffff4eabbb75b8253c4307d42
Carson Stearn <cstearn@gmail.com>
2021-08-20 20:12:19 +0000

    typo

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -10,7 +10,7 @@ import requests
 if __name__ == "__main__":
     r = requests.get(argv[1])
 
-    if r.state_code >= 400:
+    if r.status_code >= 400:
         print("Error code: {}".format(r.status_code))
     else:
         print(r.text)
