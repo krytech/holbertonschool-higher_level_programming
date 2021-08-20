6a619f2ac947d44551506b35d322aef41eb157ac
Carson Stearn <cstearn@gmail.com>
2021-08-20 20:39:12 +0000

    try & except

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -11,6 +11,9 @@ if __name__ == "__main__":
     r = requests.get(url)
     commits = r.json()
 
-    for i in range(10):
-        print("{}: {}".format(commits[i].get("sha"),
-              commits[i].get("commit").get("author").get("name")))
+    try:
+        for i in range(10):
+            print("{}: {}".format(commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
