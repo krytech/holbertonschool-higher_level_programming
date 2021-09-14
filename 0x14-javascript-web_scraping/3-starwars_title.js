864cc3f96694848b01db374c3fdfe6416fe2a94d
Carson Stearn <cstearn@gmail.com>
2021-09-14 17:15:25 +0000

    wrong link

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // Prints the title of the Star Wars movie corresponding to the given integer.
 const request = require('request');
-let url = 'https://swapi-api.hbtn.io/' + process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request(url, function (error, response, body) {
   console.log(error || JSON.parse(body).title);
 });
